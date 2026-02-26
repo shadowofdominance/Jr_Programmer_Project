a63f9e411f76d8327fa01b27c91c6a2206bfff60
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-02-26 18:21:38 +0530

    Completed learning about data persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,9 +1,11 @@
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
-
+    public Color teamColor;
+    
     private void Awake()
     {
         if (Instance != null)
@@ -14,5 +16,35 @@ public class MainManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColor = teamColor;
+        
+        string json = JsonUtility.ToJson(data);
+        
+        File.WriteAllText((Application.persistentDataPath + "/SaveFile.json"), json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/SaveFile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            
+            teamColor = data.teamColor;
+        }
     }
 }
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -16,7 +16,7 @@ public class MenuUIHandler : MonoBehaviour
 
     public void NewColorSelected(Color color)
     {
-        // add code here to handle when a color is selected
+        MainManager.Instance.teamColor = color;
     }
 
     private void Start()
@@ -24,6 +24,8 @@ public class MenuUIHandler : MonoBehaviour
         ColorPicker.Init();
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
+        
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
     }
 
     public void StartNew()
@@ -33,15 +35,22 @@ public class MenuUIHandler : MonoBehaviour
 
     public void Exit()
     {
+        MainManager.Instance.SaveColor();
 #if UNITY_EDITOR
         EditorApplication.ExitPlaymode();
 #else
             Application.Quit();
 #endif
     }
-
-    /*public void BackToMenu()
+    
+    public void SaveColorClicked()
     {
-        SceneManager.LoadScene(0);
-    }*/
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
+        ColorPicker.SelectColor(MainManager.Instance.teamColor);
+    }
 }
